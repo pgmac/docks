3c3dc0234a13e0b9e96712a54ee8c9f81448fdf2
Paul Macdonnell <pgmac@pgmac.net>
2019-03-16 21:43:43 +1000

    Break this up a bit

--- hass/bin/hass.sh
+++ hass/bin/hass.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 
+ACTION=${1:-"run"}
+APP=${2:-"homeassistant"}
+TAG=${3:-${USER}}
+
 if [ $1 = "upgrade" ]
 then
 	echo "Building the upgraded version of Hass"
 	docker stop home-assistant
 	docker rm home-assistant
 	docker pull homeassistant/home-assistant:latest
-	docker tag homeassistant/home-assistant:latest homeassistant/home-assistant:pgmac
+	docker tag homeassistant/home-assistant:latest homeassistant/home-assistant:${TAG}
 fi
 
 echo "Starting Hass"
 docker run -d \
        --restart=always \
-       --name="home-assistant" \
+       --name=${TAG} \
        -v ~/Development/home-assistant/config:/config \
        -v /etc/localtime:/etc/localtime.ro \
        --net=host \
-       homeassistant/home-assistant:pgmac
+       homeassistant/home-assistant:${TAG}
